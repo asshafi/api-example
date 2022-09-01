b4fac6d097a96717dff0d0168c2835c8ed71fc58
S M SHAFI <asshafi15@gmail.com>
2022-09-01 18:32:01 +0600

    expolre api

--- js/countries.js
+++ js/countries.js
@@ -15,10 +15,29 @@ const displayCountries = countries => {
         console.log(country);
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
-        <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}
+        <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+        <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     });
+}
+
+const loadCountryDetail = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country detail',code)
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+    .then(data=>displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    `;
 }
 
 loadCountries();
